49be6096986622202e3f3aac79db02e74f1606ef
Louis Duller <louis.mitchell.duller@gmail.com>
2021-11-30 17:01:57 -0500

    Implemented AddressGenerator

---
+++ src/main/scala/AddressGenerator.scala
@@ -0,0 +1,18 @@
+object AddressGenerator {
+  // Suppress messages to console
+  org.apache.log4j.Logger.getLogger("akka").setLevel(org.apache.log4j.Level.OFF)
+  org.apache.log4j.Logger.getLogger("org").setLevel(org.apache.log4j.Level.OFF)
+
+  private val _spark = org.apache.spark.sql.SparkSession
+    .builder()
+    .appName("Project 3")
+    .master("local")
+    .getOrCreate()
+  private val _country_city = _spark.sparkContext.textFile("input/CountryCities.csv")
+    .map(x => x.split(",")).map(x => (x(0), x(1)))
+
+  def generate(): String = {
+    val sample = _country_city.takeSample(withReplacement = true, 1)
+    s"${sample(0)._1},${sample(0)._2}"
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,9 +3,10 @@ object Main {
   def main(args: Array[String]): Unit = {
     var i = 0
     for(i <-0 to 10){
-      val test = paymentTXN.generate
-      Thread.sleep(100)
-      println(test)
+      val address = AddressGenerator.generate()
+      val test = paymentTXN.generate()
+      println(s"$address,$test")
+      Thread.sleep(2000)
     }
   }
 }
