c5f393bbf58560763b215569ae31cf7d37c18bf6
Moiya Josephs <49174334+moiyajosephs@users.noreply.github.com>
2021-12-01 11:03:28 -0500

    Create DateTimeGenerator.scala

---
+++ src/main/scala/DateTimeGenerator.scala
@@ -0,0 +1,15 @@
+import scala.util.Random
+import java.time._
+
+object DateTimeGenerator {
+  def generator (): String = {
+        val r1 = new Random()
+        val days = r1.nextInt(366)
+        val minutes = r1.nextInt(1440)
+        val dateStart = LocalDate.of(2020, 11, 30)
+        val timeStart = LocalTime.of(0, 0, 0)
+        val Date = dateStart.plusDays(days)
+        val Time = timeStart.plusMinutes(minutes)
+        "Timestamp: " + Date + " " + Time
+    }
+}
