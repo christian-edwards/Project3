3fe89056c82f899684244029550e8e77bdd17796
Moiya Josephs <49174334+moiyajosephs@users.noreply.github.com>
2021-12-01 11:16:01 -0500

    Create paymentType.scala

---
+++ src/main/scala/paymentType.scala
@@ -0,0 +1,15 @@
+object paymentType {
+  val payments = Array("card", "Internet Banking", "UPI", "Wallet")
+
+  def generate(): String ={
+    val r = new scala.util.Random(System.currentTimeMillis())
+    val x = r.nextInt(100)
+    if (x > 20){
+      return payments(r.nextInt(2))
+    }
+    else{
+      return payments(r.nextInt(4))
+    }
+
+  }
+}
