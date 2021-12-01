df82796da8cada2c257ab707f0e513f747e2b3d5
Louis Duller <louis.mitchell.duller@gmail.com>
2021-12-01 13:02:15 -0500

    Integrated QuantityTransactionGenerator into Generator

--- src/main/scala/Generator.scala
+++ src/main/scala/Generator.scala
@@ -15,8 +15,10 @@ object Generator {
   private val _country_city = _spark.sparkContext.textFile("input/CountryCities.csv")
     .map(x => x.split(",")).map(x => (x(0), x(1)))
 
+  // Call generate() from elsewhere
   def generate(): String = {
     s"${PaymentTypeGenerator.generate()}," +
+      s"${QuantityTransactionGenerator.generate()}," +
       s"${DateTimeGenerator.generate()}," +
       s"${AddressGenerator.generate()}," +
       s"${PaymentTransactionGenerator.generate()}"
@@ -68,6 +70,51 @@ object Generator {
       else{
         payments(r.nextInt(4))
       }
+    }
+  }
+
+  private object QuantityTransactionGenerator {
+    var theCount = 0  // counts # of times called, hopefully // used to generate trends ie. after a while, our business will grow
+    var theCountess :Array[Int] = new Array(100)
+    val theWarehouse: Array[String] = Array("Camera Accessories", "Car Electronics & GPS", "Cell Phones & Accessories", "Computer & Accessories", "Computers & Accessories", "Digital Cameras", "Hard Drives", "Headphones", "Home Audio", "Home Security", "Media Players & Recorders", "Memory Cards", "Mirrorless System Lenses", "Portable Bluetooth Speakers", "Power Supplies", "Projection Screens & Material", "Security Cameras", "Smart Watches", "Solid State Drives", "Speaker Cables", "Speaker Systems", "Streaming Media Players", "TV", "TV Accessories & Parts", "Video Games", "Wireless Speakers")
+    val rando = new scala.util.Random(System.currentTimeMillis)
+    var i = 0
+
+    // repeated calls without any parameters will increase the number returned by this function
+    def generate(): String = { //generate with basic trending
+      this.theCount += 1
+      i = 0
+      if (this.theCount > 25) { // end of our trend
+        this.i  = this.rando.nextInt(250) + 250 // upToRoof + floor
+      } else if (this.theCount > 10) { // small number
+        this.i  = this.rando.nextInt(150) + 100
+      } else { // smallest number
+        this.i = this.rando.nextInt(100)
+      }
+      i.toString
+    }
+
+    // parameter of type boolean will just ignore the counters
+    //    works for true or false
+    def generate(killTrend: Boolean): String = {
+      i = 0
+      this.i = this.rando.nextInt(1000)
+      i.toString
+    }
+
+    //  separate counters for each category type --declared up top--
+    def generate(category: String): String = {
+      val ID = this.theWarehouse.indexOf(category)
+      if (ID < 0) {
+        return "WTF was that again?"
+      }
+      this.theCountess(ID) += 1 // instead of generic count
+      i = 0
+      i = this.rando.nextInt(100)
+      if (this.theCountess(ID) >= 10) { // insert various conditions here
+        i += 100
+      }
+      i.toString
     }
   }
 }
