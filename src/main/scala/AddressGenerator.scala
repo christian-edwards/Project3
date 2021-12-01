77f70ead7cfab24ba282516a0c6cd664ef9a95ec
christian-edwards <christianallane@gmail.com>
2021-12-01 12:07:06 -0500

    generatev4

--- src/main/scala/AddressGenerator.scala
+++ src/main/scala/AddressGenerator.scala
@@ -1,5 +1,9 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+
 object AddressGenerator {
-  // Suppress messages to console
+  // Suppress messages to console //
+  /*
   org.apache.log4j.Logger.getLogger("akka").setLevel(org.apache.log4j.Level.OFF)
   org.apache.log4j.Logger.getLogger("org").setLevel(org.apache.log4j.Level.OFF)
 
@@ -10,8 +14,8 @@ object AddressGenerator {
     .getOrCreate()
   private val _country_city = _spark.sparkContext.textFile("input/CountryCities.csv")
     .map(x => x.split(",")).map(x => (x(0), x(1)))
-
-  def generate(): String = {
+  */
+  def generate(spark:SparkSession,_country_city:RDD[(String,String)]): String = {
     val sample = _country_city.takeSample(withReplacement = true, 1)(0)
     s"${sample._1},${sample._2}"
   }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
+
 import org.apache.spark.sql.SparkSession
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    //System.setProperty("hadoop.home.dir", "C:\\hadoop")
     org.apache.log4j.Logger.getLogger("akka").setLevel(org.apache.log4j.Level.OFF)
     org.apache.log4j.Logger.getLogger("hive").setLevel(org.apache.log4j.Level.OFF)
     org.apache.log4j.Logger.getLogger("org").setLevel(org.apache.log4j.Level.OFF)
@@ -14,14 +15,18 @@ object Main {
       .config("spark.master", "local")
       .enableHiveSupport()
       .getOrCreate()
-    println("created spark session")
+    //println("created spark session")
 
     val website = spark.sparkContext.textFile("input/websites.csv")
     val website_map = website.map(x=>x.split(",")).map(x=>x(0))
 
+    val _country_city = spark.sparkContext.textFile("input/CountryCities.csv")
+      .map(x => x.split(",")).map(x => (x(0), x(1)))
+
     var i = 0
     for(i <-0 to 10){
       var output = ""
+      output += AddressGenerator.generate(spark,_country_city) + ","
       output += paymentType.generate + ","
       output += websiteGen.generate(spark,website_map) + ","
       output += DateTimeGenerator.generate + ","
