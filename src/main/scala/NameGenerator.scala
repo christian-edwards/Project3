618c48d413e736105bc96d54dcaf15655a5005fe
christian-edwards <christianallane@gmail.com>
2021-12-01 14:34:41 -0500

    generatev6.1

--- src/main/scala/NameGenerator.scala
+++ src/main/scala/NameGenerator.scala
@@ -4,17 +4,7 @@ import org.apache.spark.sql.SparkSession
 import java.util
 
 object NameGenerator {
-  /*
-  var outList = new util.ArrayList[String]();
 
-  val bufferedSource = io.Source.fromFile("input/namesFile.csv")
-  for (line <- bufferedSource.getLines) {
-    val cols = line.split(",").map(_.trim)
-    val a= s"${cols(0)},${cols(1)}"
-    outList.add(a)
-  }
-  bufferedSource.close
-*/
   def generate(spark:SparkSession,namesMap:RDD[(String,String)]): String = {
     val randomName = namesMap.takeSample(withReplacement = true, 1)(0)
     s"${randomName._1},${randomName._2}"
