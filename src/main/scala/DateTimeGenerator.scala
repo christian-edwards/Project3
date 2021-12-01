5bed8fd0942b5ebc20140b7e09c265b713936cc6
christian-edwards <christianallane@gmail.com>
2021-12-01 11:08:58 -0500

    generatev2

--- src/main/scala/DateTimeGenerator.scala
+++ src/main/scala/DateTimeGenerator.scala
@@ -2,7 +2,7 @@ import scala.util.Random
 import java.time._
 
 object DateTimeGenerator {
-  def generator (): String = {
+  def generate (): String = {
         val r1 = new Random()
         val days = r1.nextInt(366)
         val minutes = r1.nextInt(1440)
@@ -10,6 +10,6 @@ object DateTimeGenerator {
         val timeStart = LocalTime.of(0, 0, 0)
         val Date = dateStart.plusDays(days)
         val Time = timeStart.plusMinutes(minutes)
-        "Timestamp: " + Date + " " + Time
+        Date + " " + Time
     }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,7 @@ object Main {
     var i = 0
     for(i <-0 to 10){
       var output = ""
+      output += DateTimeGenerator.generate + ","
       output += quantityTXN.generate + ","
       output += paymentTXN.generate
       Thread.sleep(100)
