a7453b64b839b6e07bf56b190a96bd3436198334
cselling <42050244+cselling@users.noreply.github.com>
2021-12-15 09:54:11 -0800

    Weighted Quantity
    Added weights to quantity output based on which category was selected
    Removed the counters and any influence that had because decoupled from date, it was just noise

--- src/main/scala/Generator.scala
+++ src/main/scala/Generator.scala
@@ -118,14 +118,20 @@ object Generator {
       if (ID < 0) {
         return "Error: Category Not Found"
       }
-      this.theCountess(ID) += 1 // instead of generic count
       i = 0
-      if (this.theCountess(ID) > 25) { // copy of generate()  // insert various conditions here
-        this.i  = this.rando.nextInt(250) + 250 // upToRoof + floor
-      } else if (this.theCountess(ID) > 10) {
-        this.i  = this.rando.nextInt(150) + 100
-      } else {
-        this.i = this.rando.nextInt(100)
+      category match {
+        case "Computers & Accessories" => // weighted to be larger sales
+          this.i = this.rando.nextInt(375) + 375 // upToRoof + floor
+        case "Home Audio" => // weighted to be larger sales
+          this.i = this.rando.nextInt(375) + 375
+        case "Hard Drives" =>
+            this.i = this.rando.nextInt(99) + 1
+        case "Home Security" => // weighted to be constant sales
+          this.i = this.rando.nextInt(25) + 25
+        case "TV" => // weighted to be smaller sales
+          this.i = this.rando.nextInt(25) + 25
+        case default =>
+          this.i = this.rando.nextInt(150) + 100
       }
       i.toString
     }
